77101d0f3cb4f0eabc6d0736e9b1d354c37da07a
Liferoijrm <pedroextrer@gmail.com>
2025-06-13 19:50:31 -0300

    :sparkles: feat: tela inicial de cadastro (imagem e campos com mudanças pendentes)

---
+++ projeto_cjr_frontend/src/app/register/page.tsx
@@ -0,0 +1,60 @@
+export default function Home() {
+  return (
+    /* conteiner principal da tela */
+    <div className="flex h-screen"> 
+        {/* lado esquerdo da tela */}
+        <div className= 
+            "w-4/7 h-full bg-cover bg-center" 
+            style={{ backgroundImage: "url('/imagens/raisedhand.jpg')" }}
+        ></div>
+        {/* lado direito da tela */}
+        <div className="w-3/7 flex flex-col items-center h-full bg-blue-900 p-16" /*style={{ backgroundImage: "url('/imagens/bluebackground.jpg')"}} */>
+            <h1 className="text-center font-bold text-5xl p-5 text-white">Cadastre-se!</h1>
+            <form className="flex flex-col items-center w-full">
+                <input 
+                    className="rounded-lg font-normal bg-white hover:bg-gray-100 focus:bg-gray-100 w-110 h-12 m-2 px-2 placeholder-gray-400 focus:outline-none" 
+                    placeholder="Nome"
+                    type="name"
+                    name="nome"
+                    required
+                ></input>
+                <input 
+                    className="rounded-lg font-normal bg-white hover:bg-gray-100 focus:bg-gray-100 w-110 h-12 m-2 px-2 placeholder-gray-400 focus:outline-none" 
+                    placeholder="Email"
+                    type="email"
+                    name="email"
+                    required   
+                ></input>
+                <input 
+                    className="rounded-lg font-normal bg-white hover:bg-gray-100 focus:bg-gray-100 w-110 h-12 m-2 px-2 placeholder-gray-400 focus:outline-none" 
+                    placeholder="Senha"
+                    type="password"
+                    name="senha"
+                    required
+                ></input>
+                {/*talvez modificar os campos de curso/departamento para um seletor futuramente*/}
+                <input 
+                    className="rounded-lg font-normal bg-white hover:bg-gray-100 focus:bg-gray-100 w-110 h-12 m-2 px-2 placeholder-gray-400 focus:outline-none" 
+                    placeholder="Curso"
+                    type="text"
+                    name="curso"
+                    required
+                ></input>
+                <input 
+                    className="rounded-lg font-normal bg-white hover:bg-gray-100 focus:bg-gray-100 w-110 h-12 m-2 px-2 placeholder-gray-400 focus:outline-none" 
+                    placeholder="Departamento"
+                    type="text"
+                    name="departamento"
+                    required
+                ></input>
+                <button 
+                    className="rounded-md bg-blue-600 hover:bg-blue-700 active:bg-blue-800 text-white font-bold w-30 h-12 py-2 px-4 mx-10 my-10"
+                    type="submit"
+                >
+                    Criar Conta
+                </button>
+            </form>
+        </div>
+    </div>
+  );
+}
