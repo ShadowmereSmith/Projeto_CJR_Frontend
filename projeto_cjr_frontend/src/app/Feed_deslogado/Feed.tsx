30563985d8009709aa077977d753f144f6320250
Moisés Gibson <mvermelho95@gmail.com>
2025-06-15 10:32:39 -0300

    Começando o Feed Deslogado

---
+++ projeto_cjr_frontend/src/app/Feed_deslogado/Feed.tsx
@@ -0,0 +1,11 @@
+export default function FeedDeslogado() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-8 gap-16 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] items-center">
+        <h1 className="text-2xl font-bold">Feed Deslogado</h1>
+        <p className="text-lg">Bem-vindo ao feed deslogado!</p>
+        <p className="text-sm text-gray-500">Aqui você pode ver as últimas atualizações.</p>
+      </main>
+    </div>
+  );
+} 
